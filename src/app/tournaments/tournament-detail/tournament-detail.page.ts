5fbaa37ea637e1d255bea57d35aa104ff31e058b
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-11 22:05:48 +0000

    wired data using tournaments service and using navigation between tournaments and tournament detail

--- src/app/tournaments/tournament-detail/tournament-detail.page.ts
+++ src/app/tournaments/tournament-detail/tournament-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TournamentI } from '../tournament.model';
+import { ActivatedRoute } from '@angular/router';
+import { TournamentsService } from '../tournaments.service';
 
 @Component({
   selector: 'app-tournament-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tournament-detail.page.scss'],
 })
 export class TournamentDetailPage implements OnInit {
+  loadedTournament: TournamentI;
 
-  constructor() { }
+  constructor(private activedRoute: ActivatedRoute, private tournamentsService: TournamentsService) { }
 
   ngOnInit() {
+    this.activedRoute.paramMap.subscribe( paramMap => {
+      if(!paramMap.has('tournamentId')) {
+        //redirect
+        return;
+      }
+
+      const tournamentId = paramMap.get('tournamentId');
+      this.loadedTournament = this.tournamentsService.getTournamentById(
+        tournamentId
+      );
+    });
   }
 
 }
--- src/app/tournaments/tournaments.page.ts
+++ src/app/tournaments/tournaments.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TournamentI } from './tournament.model';
+import { TournamentsService } from './tournaments.service';
 
 @Component({
   selector: 'app-tournaments',
@@ -7,26 +8,11 @@ import { TournamentI } from './tournament.model';
   styleUrls: ['./tournaments.page.scss'],
 })
 export class TournamentsPage implements OnInit {
-  tournaments: TournamentI[] = [
-    {
-      end_date: '23/03/2020',
-      id: 1,
-      start_date: '18/02/2020',
-      title: 'First Tournament',
-      imageUrl:
-        'https://cdn1.tahoedonner.com/wp-content/uploads/2018/05/11193919/TennisHeader10.jpg',
-    },
-    {
-      end_date: '12/05/2020',
-      id: 2,
-      start_date: '23/03/2020',
-      title: 'Second Tournament',
-      imageUrl:
-        'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS1AY-pRkhCeNHEs8UpClYBOlMcF9-igLQaFNfD3_vAyonjyC08',
-    },
-  ];
+  tournaments: TournamentI[];
 
-  constructor() {}
+  constructor(private tournamentsService: TournamentsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.tournaments = this.tournamentsService.getAllTournaments();
+  }
 }
