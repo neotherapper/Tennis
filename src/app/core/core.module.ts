44a3a036f712a44192b66f65171e270c5a12ea6e
Georgios Pilitsoglou <george.pilitsoglou@gmail.com>
2020-03-12 15:21:44 +0000

    created layout component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
 
 
+const components = [
+  LayoutComponent
+]
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [components],
+  imports: [CommonModule, IonicModule, RouterModule],
+  exports: [components],
 })
-export class CoreModule { }
+export class CoreModule {}
